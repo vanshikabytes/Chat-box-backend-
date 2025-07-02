d7f3aecbbfb5673aeab0f6d62c7e550a5d96bc38
Vanshika vijay <vanshikavijay0304@gmail.com>
2025-07-02 08:12:00 +0530

    fix: update CORS for production

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server,{
   cors:{
-    origin:"http://localhost:3000",
+    origin:"https://box-chat.netlify.app/",
     credentials: true,
   }
 })
